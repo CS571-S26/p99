6414ac688cc03c4a0d5a6bc8fbcfd763b07b381d
Jared Kramer <jkramer321@hotmail.com>
2026-04-07 14:27:59 -0500

    navbar comp

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link, NavLink } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+            <Link className="navbar-brand fw-bold" to="/">Ghost Job Detector</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/" end>Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/reports">Reports</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
