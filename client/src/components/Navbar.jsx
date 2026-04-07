1c45f5872aa68ddcb8a31349112ecb40dfe263a3
Jared Kramer <jkramer321@hotmail.com>
2026-04-07 15:18:50 -0500

    full screen and adjustable

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+        <nav className="navbar navbar-dark bg-dark px-4 d-flex">
             <Link className="navbar-brand fw-bold" to="/">Ghost Job Detector</Link>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav ms-auto">
+            <div className="d-flex">
+                <ul className="navbar-nav flex-row ms-auto gap-1 gap-md-3 gap-lg-4">
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/" end>Home</NavLink>
                     </li>
